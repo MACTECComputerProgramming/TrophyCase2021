b7272a1f157763cb9c869cec86d02b8bd5bd0cb2
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2021-04-08 11:05:33 -0500

    Start on UI

---
+++ Trophy Case/Assets/ButtonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager unityScene = new SceneManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Browse()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
