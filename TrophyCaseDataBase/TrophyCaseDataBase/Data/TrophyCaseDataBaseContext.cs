e7dedf6822d459577389d7aeb28055ffbcdb4aed
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2021-04-20 10:21:10 -0500

    The project, with the works!

---
+++ TrophyCaseDataBase/TrophyCaseDataBase/Data/TrophyCaseDataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrophyCaseDataBase.Models;
+
+namespace TrophyCaseDataBase.Data
+{
+    public class TrophyCaseDataBaseContext : DbContext
+    {
+        public TrophyCaseDataBaseContext (DbContextOptions<TrophyCaseDataBaseContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<TrophyCaseDataBase.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ TrophyCaseDataBase/TrophyCaseDataBase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrophyCaseDataBase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
